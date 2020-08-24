cc974250fb3e11f5ef3a7fb5be63d36a01fecc2f
LuuVanCui <61924545+LuuVanCui@users.noreply.github.com>
2020-08-24 09:18:28 +0700

    login with google

---
+++ index.js
@@ -0,0 +1,64 @@
+// index.js
+
+/*  EXPRESS */
+
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const passport = require('passport');
+var userProfile;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '820849889489-2l62hgnerhtneg65uuq6lbkehhfgbhg2.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = 'oiaSW7ok5pKyIGNz9_7TsLCi';
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+// End Passport
+
+//router
+app.get('/', function(req, res) {
+  res.render('pages/auth');
+});
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
+// run app
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
